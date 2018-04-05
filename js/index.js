b5d082dd874ef8b394c6ffd4fe652e65bc087e06
Dmytro Malakhov <coast93@mail.ru>
2018-04-05 17:53:29 +0000

    ready to start

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const str = 'Dima';
-const anAge = 24;
+const user = prompt('Enter your name');
+const age = parseFloat(prompt('Enter your age'));
 const married = true;
 const lawViolation = undefined;
 const empty = null;
@@ -8,22 +8,18 @@ const someSym = 'Symbol()';
 const bye = 'Goodbye';
 
 document.write('7 types of data:' + '<br><br>');
-document.write('The string is: ' + '\'' + str + '\'' +';<br>');
-document.write('The number is: ' + anAge + ';<br>'); 
+document.write('The string is: ' + '\'' + user + '\'' +';<br>');
+document.write('The number is: ' + age + ';<br>'); 
 document.write('The boolean (true/false) is: ' + married + ';<br>'); 
 document.write('The undefined is: ' + lawViolation + ';<br>');
 document.write('The null is: ' + empty + ';<br>'); 
 document.write('The object is: ' + someObj + ';<br>');
 document.write('The symbol is: ' + someSym + ';<br><br><br>')
 
-
-const user = prompt('Enter your name');
 document.write('Hello ' + user + '<br>');
-
 let greet = 'Hello ' + user;
 alert(greet);
 
-const age = parseFloat(prompt('Enter your age'));
 const nextAge = age + 1;
 document.write('Next year you will be ' + nextAge + '<br>');
 
@@ -32,8 +28,11 @@ function rightAge () {
         document.write('Your are a child');
     } else if ( age <= 75 ){
         document.write('Your are an Adult');
-    } else 
-        console.log(bye); 
+    } else {
+        document.write('See  a console'); 
+        console.log(bye);
+    }
+         
 }
 
 rightAge(age);
